2bceed009b42253314fd53ca7920abb077959cc7
Sorin Gore <soringoredev@gmail.com>
2025-09-22 16:21:42 +0300

    Add new example method class

---
+++ src/PrimulExempluMetode.java
@@ -0,0 +1,20 @@
+public class PrimulExempluMetode {
+    public static void main(String[] args) {
+
+        arataMesaj();
+        PrimulExempluMetode obiect = new PrimulExempluMetode();
+        obiect.arataMesaj2();
+        //arataMesaj2();
+
+    }
+
+    public static void arataMesaj() {
+        System.out.println("Mesaj de felicitare.");
+    }
+
+    public void arataMesaj2() {
+        System.out.println("Mesaj de felicitare.");
+    }
+
+
+}
